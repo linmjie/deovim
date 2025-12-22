dd3922df6ee12ed669b74ef98b57ca734aac5652
linmjie <linmgjie@gmail.com>
2025-12-22 15:45:58 -0500

    added a "main" harpoon list

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -8,3 +8,14 @@ for i = 1, 9 do
 end
 
 vim.keymap.set('n', "<leader>o", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+local main = "main"
+
+vim.keymap.set('n', "<leader>maa", function() harpoon:list(main):add() end)
+
+for i = 1, 9 do
+    vim.keymap.set('n', "<leader>ma" .. i, function() harpoon:list(main):replace_at(i) end)
+    vim.keymap.set('n', "<leader>m" .. i, function() harpoon:list(main):select(i) end)
+end
+
+vim.keymap.set('n', "<leader>O", function() harpoon.ui:toggle_quick_menu(harpoon:list(main)) end)
